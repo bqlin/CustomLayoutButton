eade78b2723ddc1649a6276f66cf314466fd75e7
bqlin <bqlins@163.com>
2018-10-31 09:43:53 +0800

    添加文档注释

--- CustomLayoutButton/CustomLayoutButton.h
+++ CustomLayoutButton/CustomLayoutButton.h
@@ -8,19 +8,30 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ 自定义布局类型
+ */
 typedef NS_ENUM(NSInteger, CustomButtonLabelImageLayout) {
+    // 标签、图片从左到右布局
     CustomButtonLabelImageLayoutLeftToRight = 0,
+    // 标签、图片从右到左布局
     CustomButtonLabelImageLayoutRightToLeft,
+    // 标签、图片从上到下布局
     CustomButtonLabelImageLayoutTopToBottom,
+    // 标签、图片从下到上布局
     CustomButtonLabelImageLayoutBottomToTop
 };
 
 @interface CustomLayoutButton : UIButton
 
-/// 布局类型
+/**
+ 布局类型
+ */
 @property (nonatomic, assign) IBInspectable NSInteger layoutType;
 
-/// 标签与图片间隔
+/**
+ 标签与图片间隔
+ */
 @property (nonatomic, assign) IBInspectable CGFloat labelImageSpacing;
 
 @end
